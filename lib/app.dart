778b9b1c436063f9e84625ff1e29455354041981
akmatoff <azim.akmatov1@gmail.com>
2020-03-30 22:02:25 +0600

    empty home screen

--- lib/app.dart
+++ lib/app.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
+import 'views/home.dart';
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
+  @override
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<App> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Home(),
+        theme: ThemeData(primaryColor: Colors.limeAccent));
   }
 }
