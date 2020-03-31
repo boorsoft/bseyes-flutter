de0192a3e81149351a24b641608ef96f4880ec19
akmatoff <azim.akmatov1@gmail.com>
2020-03-31 22:29:57 +0600

    added bg

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -3,6 +3,13 @@ import 'package:flutter/material.dart';
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Container(
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage("assets/images/bg2.jpg"),
+                  fit: BoxFit.cover)),
+          child: null),
+    );
   }
 }
