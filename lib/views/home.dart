0b60ce1c6dfbf9ce7b125301f9785379f4501cc9
akmatoff <azim.akmatov1@gmail.com>
2020-04-05 14:31:56 +0600

    tried to read json from restapi

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,15 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  fetchData() async {
+    final response =
+        await http.get("https://bseyes-restapi--akmatoff.repl.co/api/teachers");
+    if (response.statusCode == 200) {
+      var data = json.decode(response.body);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
+        body: Stack(
+      children: <Widget>[
+        Container(
           decoration: BoxDecoration(
               image: DecorationImage(
-                  image: AssetImage("assets/images/bg2.jpg"),
+                  image: AssetImage("assets/images/bg.jpg"),
                   fit: BoxFit.cover)),
-          child: null),
-    );
+        ),
+        Center(
+          child: Container(
+            margin: EdgeInsets.only(left: 60.0, right: 60.0),
+            height: 350.0,
+            decoration: BoxDecoration(
+                color: Colors.white, borderRadius: BorderRadius.circular(20.0)),
+          ),
+        ),
+      ],
+    ));
   }
 }
