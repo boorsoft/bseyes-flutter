dda5e011c8f4ccf5620dd7c30f8a312d35e6cb3e
akmatoff <azim.akmatov1@gmail.com>
2020-04-08 16:27:32 +0600

    Get subjects and teachers data from API

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -2,53 +2,51 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-Future fetchSubjects(http.Client client) async {
-  final response =
-      await client.get('http://bseyes-restapi--akmatoff.repl.co/api/teachers');
-
-  return response.body;
-}
-
 class Home extends StatefulWidget {
   @override
   _HomeState createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
+  String subjectsUrl = "https://bseyes-restapi--akmatoff.repl.co/api/subjects";
+  String teachersUrl = "https://bseyes-restapi--akmatoff.repl.co/api/teachers";
   List subjects;
+  List teachers;
+
+  @override
+  void initState() {
+    super.initState();
+    this.getSubjects();
+  }
+
+  Future<String> getSubjects() async {
+    var subjectsResponse = await http.get(Uri.encodeFull(subjectsUrl),
+        headers: {"Accept": "application/json"});
+
+    var teachersResponse = await http.get(Uri.encodeFull(teachersUrl),
+        headers: {"Accept": "application/json"});
+
+    print(subjectsResponse.body);
+
+    setState(() {
+      subjects = json.decode(utf8.decode(subjectsResponse.bodyBytes));
+      teachers = json.decode(utf8.decode(teachersResponse.bodyBytes));
+    });
+
+    return "OK!";
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: FutureBuilder(
-            future: fetchSubjects(http.Client()),
-            builder: (context, snapshot) {
-              subjects = json.decode(snapshot.data.toString());
-              print(subjects[0]['first_name']);
-              if (snapshot.hasData) {
-                return Stack(
-                  children: <Widget>[
-                    Container(
-                      decoration: BoxDecoration(
-                          image: DecorationImage(
-                              image: AssetImage("assets/images/bg.jpg"),
-                              fit: BoxFit.cover)),
-                    ),
-                    Center(
-                      child: Container(
-                          margin: EdgeInsets.only(left: 60.0, right: 60.0),
-                          height: 350.0,
-                          decoration: BoxDecoration(
-                              color: Colors.white,
-                              borderRadius: BorderRadius.circular(20.0)),
-                          child: Text(subjects[0]['first_name'],
-                              style: TextStyle(fontSize: 20.0))),
-                    ),
-                  ],
-                );
-              } else {
-                return Center(child: CircularProgressIndicator());
-              }
+        body: ListView.builder(
+            itemCount: subjects.length,
+            itemBuilder: (BuildContext context, int index) {
+              return Container(
+                  height: 60.0,
+                  child: Center(
+                    child: Text(subjects[index]['sub_name']),
+                  ));
             }));
   }
 }
