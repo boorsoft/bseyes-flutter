6b5babcdce7fa489bcf75224e6adcc648cea6c4e
Baichik <56549703+Baichik@users.noreply.github.com>
2020-04-16 21:30:43 +0600

    update

---
+++ lib/services/teachers_service.dart
@@ -0,0 +1,27 @@
+import 'dart:convert';
+
+import 'package:bseyes_flutter/models/teacher_model.dart';
+import 'package:http/http.dart';
+
+import '../models/teacher_model.dart';
+
+
+class TechersService {
+  final String teachersUrl = "https://bseyes-restapi--akmatoff.repl.co/api/teachers";
+
+  Future<List<Teacher>> getTeachers() async{
+
+    Response res = await get(
+      teachersUrl);
+    
+    if(res.statusCode == 200){
+      List<dynamic> body = jsonDecode(utf8.decode(res.bodyBytes));
+
+      List<Teacher> teachers = body
+          .map((dynamic item) => Teacher.fromJson(item))
+          .toList();
+
+    return teachers;
+    }
+  }
+}
