53dfbf2d5834e45d3f991dcc827c4290afa8d74e
akmatoff <azim.akmatov1@gmail.com>
2020-05-05 13:42:35 +0600

    Added a form

--- lib/views/login.dart
+++ lib/views/login.dart
@@ -11,9 +11,15 @@ class LoginState extends State<Login> {
   final usernameController = TextEditingController();
   final passwordController = TextEditingController();
 
+  final formKey = GlobalKey<FormState>();
+
   logIn(String username, String password) async {
-    Navigator.of(context)
-        .push(MaterialPageRoute(builder: (BuildContext context) => Subjects()));
+    if (formKey.currentState.validate()) {
+      formKey.currentState.save();
+      print(username + "," + password);
+    } else {
+      print('Ошибка');
+    }
   }
 
   @override
@@ -21,8 +27,8 @@ class LoginState extends State<Login> {
     return Scaffold(
         body: Center(
             child: Container(
-                padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 30.0),
-                height: 400.0,
+                padding: EdgeInsets.symmetric(horizontal: 50.0, vertical: 30.0),
+                constraints: BoxConstraints(minHeight: 300.0, maxHeight: 450.0),
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   crossAxisAlignment: CrossAxisAlignment.start,
@@ -33,78 +39,98 @@ class LoginState extends State<Login> {
                       style: headingTextStyle,
                     )),
                     SizedBox(height: 20.0),
-                    Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text(
-                            'Логин',
-                            style: defaultTextStyleBold,
+                    Form(
+                        key: formKey,
+                        child: Column(children: <Widget>[
+                          Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Text(
+                                  'Логин',
+                                  style: defaultTextStyleBold,
+                                ),
+                                SizedBox(height: 20.0),
+                                Container(
+                                    child: TextFormField(
+                                        controller: usernameController,
+                                        validator: (input) => input == ''
+                                            ? 'Это поле обязательное'
+                                            : null,
+                                        decoration: InputDecoration(
+                                            labelText: 'Введите логин...',
+                                            focusedBorder: OutlineInputBorder(
+                                                borderSide: BorderSide(
+                                                    color: Theme.of(context)
+                                                        .primaryColor,
+                                                    style: BorderStyle.solid,
+                                                    width: 2.0)),
+                                            errorBorder: OutlineInputBorder(
+                                                borderSide: BorderSide(
+                                                    color: Colors.red,
+                                                    style: BorderStyle.solid,
+                                                    width: 2.0)),
+                                            enabledBorder: OutlineInputBorder(
+                                                borderSide: BorderSide(
+                                                    color: Colors.black87,
+                                                    style: BorderStyle.solid,
+                                                    width: 2.0)))))
+                              ]),
+                          SizedBox(height: 10.0),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Text(
+                                'Пароль',
+                                style: defaultTextStyleBold,
+                              ),
+                              SizedBox(height: 20.0),
+                              Container(
+                                  child: TextFormField(
+                                      controller: passwordController,
+                                      validator: (input) => input == ''
+                                          ? 'Это поле обязательное'
+                                          : null,
+                                      obscureText: true,
+                                      decoration: InputDecoration(
+                                          labelText: 'Введите пароль...',
+                                          focusedBorder: OutlineInputBorder(
+                                              borderSide: BorderSide(
+                                                  color: Theme.of(context)
+                                                      .primaryColor,
+                                                  style: BorderStyle.solid,
+                                                  width: 2.0)),
+                                          errorBorder: OutlineInputBorder(
+                                              borderSide: BorderSide(
+                                                  color: Colors.red,
+                                                  style: BorderStyle.solid,
+                                                  width: 2.0)),
+                                          enabledBorder: OutlineInputBorder(
+                                              borderSide: BorderSide(
+                                                  color: Colors.black87,
+                                                  style: BorderStyle.solid,
+                                                  width: 2.0)))))
+                            ],
                           ),
-                          SizedBox(height: 20.0),
                           Container(
-                              child: TextField(
-                                  controller: usernameController,
-                                  decoration: InputDecoration(
-                                      labelText: 'Введите логин...',
-                                      focusedBorder: OutlineInputBorder(
-                                          borderSide: BorderSide(
-                                              color: Theme.of(context)
-                                                  .primaryColor,
-                                              style: BorderStyle.solid,
-                                              width: 3.0)),
-                                      enabledBorder: OutlineInputBorder(
-                                          borderSide: BorderSide(
-                                              color: Colors.black87,
-                                              style: BorderStyle.solid,
-                                              width: 3.0)))))
-                        ]),
-                    SizedBox(height: 10.0),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text(
-                          'Пароль',
-                          style: defaultTextStyleBold,
-                        ),
-                        SizedBox(height: 20.0),
-                        Container(
-                            child: TextField(
-                                controller: passwordController,
-                                obscureText: true,
-                                decoration: InputDecoration(
-                                    labelText: 'Введите пароль...',
-                                    focusedBorder: OutlineInputBorder(
-                                        borderSide: BorderSide(
-                                            color:
-                                                Theme.of(context).primaryColor,
-                                            style: BorderStyle.solid,
-                                            width: 3.0)),
-                                    enabledBorder: OutlineInputBorder(
-                                        borderSide: BorderSide(
-                                            color: Colors.black87,
-                                            style: BorderStyle.solid,
-                                            width: 3.0)))))
-                      ],
-                    ),
-                    Container(
-                      width: double.infinity,
-                      padding:
-                          EdgeInsets.symmetric(horizontal: 0, vertical: 15.0),
-                      child: RaisedButton(
-                        onPressed: () => logIn(
-                            usernameController.text, passwordController.text),
-                        padding: EdgeInsets.all(10.0),
-                        color: Colors.black87,
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(15.0)),
-                        child: Text('ВОЙТИ',
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold,
-                                fontSize: 15.0,
-                                letterSpacing: 3.5)),
-                      ),
-                    )
+                            width: double.infinity,
+                            padding: EdgeInsets.symmetric(
+                                horizontal: 0, vertical: 15.0),
+                            child: RaisedButton(
+                              onPressed: () => logIn(usernameController.text,
+                                  passwordController.text),
+                              padding: EdgeInsets.all(10.0),
+                              color: Colors.black87,
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(15.0)),
+                              child: Text('ВОЙТИ',
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 15.0,
+                                      letterSpacing: 3.5)),
+                            ),
+                          )
+                        ])),
                   ],
                 ))));
   }
